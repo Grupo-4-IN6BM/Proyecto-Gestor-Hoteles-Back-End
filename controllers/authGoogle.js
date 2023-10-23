c4c09ebd985a1eb182c34f04fe7c344348e430c7
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2023-10-22 18:37:28 -0600

    Actualizando al usuario que se loguee si ya se ha logueado 1 vez

--- controllers/authGoogle.js
+++ controllers/authGoogle.js
@@ -6,25 +6,29 @@ const { generarJWT } = require('../helpers/generar-jwt');
 
 const postUsuarioGoogle = async (req = express.request, res = express.response) => {
   const body = req.body;
-  
-  const validUser = await Usuario.findOne({correo: body.correo})
 
-  if(validUser){
+  const validUser = await Usuario.findOne({ correo: body.correo })
+
+  if (validUser) {
     const token = await generarJWT(validUser._id, validUser.rol)
-    res.json({validUser, token})
-  }else{
-  let newUser = new Usuario({
-    nombre: body.nombre,
-    correo: body.correo,
-    rol: "ROL_CLIENTE",
-    img: body.img
-  });
-  const token = await generarJWT(newUser._id, newUser.rol)
-  const reservacionAuto = new reservacion({ usuario: newUser._id })
-  await reservacionAuto.save();
-  await newUser.save();
-  res.json({newUser, token})
-}
+    const updateUser = await Usuario.findByIdAndUpdate(validUser._id, {
+      nombre: body.nombre,
+      img: body.img
+    })
+    res.json({ updateUser, token })
+  } else {
+    let newUser = new Usuario({
+      nombre: body.nombre,
+      correo: body.correo,
+      rol: "ROL_CLIENTE",
+      img: body.img
+    });
+    const token = await generarJWT(newUser._id, newUser.rol)
+    const reservacionAuto = new reservacion({ usuario: newUser._id })
+    await reservacionAuto.save();
+    await newUser.save();
+    res.json({ newUser, token })
+  }
 }
 
 module.exports = {
